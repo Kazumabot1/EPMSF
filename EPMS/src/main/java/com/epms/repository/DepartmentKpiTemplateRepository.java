e205e056b394e50ab919a5add611dc30bc6faa93
AliceWaddy10 <yuwaddy1010@gmail.com>
2026-05-29 22:43:45 +0630

    error fix 7

--- EPMS/src/main/java/com/epms/repository/DepartmentKpiTemplateRepository.java
+++ EPMS/src/main/java/com/epms/repository/DepartmentKpiTemplateRepository.java
@@ -3,13 +3,25 @@ package com.epms.repository;
 import com.epms.entity.DepartmentKpiTemplate;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface DepartmentKpiTemplateRepository extends JpaRepository<DepartmentKpiTemplate, Integer> {
-    Optional<DepartmentKpiTemplate> findDetailById(Integer id);
+    @EntityGraph(attributePaths = {
+            "rows", "rows.kpiCategory", "rows.kpiUnit", "rows.kpiItem",
+            "departments", "departments.department",
+            "createdByUser"
+    })
+    @Query("SELECT t FROM DepartmentKpiTemplate t WHERE t.id = :id")
+    Optional<DepartmentKpiTemplate> findDetailById(@Param("id") Integer id);
 
-    @EntityGraph(attributePaths = {"departments", "departments.department"})
+    @EntityGraph(attributePaths = {
+            "rows", "rows.kpiCategory", "rows.kpiUnit", "rows.kpiItem",
+            "departments", "departments.department",
+            "createdByUser"
+    })
     List<DepartmentKpiTemplate> findAllByOrderByCreatedAtDesc();
 }
