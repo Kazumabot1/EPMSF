feb5b3edfcdae581fc3f94217fca726c30410838
Lynn <hirata806@gmail.com>
2026-06-01 01:27:33 +0630

    remove unecessary

--- EPMS/src/main/java/com/epms/repository/DepartmentKpiScoreRepository.java
+++
@@ -1,7 +0,0 @@
-package com.epms.repository;
-
-import com.epms.entity.DepartmentKpiScore;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DepartmentKpiScoreRepository extends JpaRepository<DepartmentKpiScore, Integer> {
-}
